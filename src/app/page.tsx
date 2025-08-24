f5fab2e2765ed25daab10f055ba1c30c73a84872
Raj Kumar sharma <rajkumarbgs851211@gmail.com>
2025-08-24 18:14:49 +0000

    Products show nhi ho Raha aisa karo ki mere database me 20 demo product

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { ProductCard } from '@/components/product-card';
-import { categories, products as mockProducts, Product } from '@/lib/mock-data';
+import { categories, Product } from '@/lib/mock-data';
 import { Card, CardContent } from '@/components/ui/card';
 import { getProducts } from '@/lib/firebase/firestore';
 
