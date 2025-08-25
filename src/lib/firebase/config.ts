e51adc3452a3f0f2257dfa62bd9d26533aa7f725
Raj Kumar sharma <rajkumarbgs851211@gmail.com>
2025-08-25 12:15:23 +0000

    .Env me meara sara keys add karo pura Code sahi se kaam kare please GEMI

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyCWpvks_5q1nSBhsrYlNLIRX9UBZ-ZkbXA",
-  authDomain: "bazaarika-lite.firebaseapp.com",
-  projectId: "bazaarika-lite",
-  storageBucket: "bazaarika-lite.appspot.com",
-  messagingSenderId: "497294677028",
-  appId: "1:497294677028:web:d6500602307f6d462c74b1",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
