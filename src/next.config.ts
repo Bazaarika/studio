60ade1d1176d03a80801351de63227be06b2e8a0
Raj Kumar sharma <rajkumarbgs851211@gmail.com>
2025-08-31 17:32:42 +0000

    Build command npx next-on-pages Deploy command npx wrangler deploy
    23:

---
+++ src/next.config.ts
@@ -0,0 +1,42 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'bazaarika.in',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push('firebase-admin');
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
